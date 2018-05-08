dd6f5ececdd1f1285ea4c8c01fbeb32c5dec7174
Adrian Eguez <eadepto@hotmail.com>
2018-05-08 15:11:12 -0500

    propiedades y metodos

--- SpoilerApp6/src/app/app.component.ts
+++ SpoilerApp6/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Selector o etiqueta
+  templateUrl: './app.component.html', // Estructura
+  styleUrls: ['./app.component.css'] // Estilos
 })
+
 export class AppComponent {
-  title = 'app';
+  nombre = 'Adrian';
+  apellido = 'Eguez';
+  edad = 28;
+  sueldo = 1;
+  fechaNacimiento = new Date('1989-06-10');
+  nombreBoton = 'Cambiar color';
+  nombreClase = 'sa-titulo';
+
+  cambiarClaseDeTitulo() {
+    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+    if (nombreClaseEsTitulo) {
+      this.nombreClase = 'sa-subtitulo';
+    } else {
+      this.nombreClase = 'sa-titulo';
+    }
+  }
 }
+
+
+
+
