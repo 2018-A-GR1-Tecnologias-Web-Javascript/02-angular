7e54ff0bc359f7b8e8cd24edf09f7f42c3a41c56
Adrian Eguez <eadepto@hotmail.com>
2018-05-18 16:37:08 -0500

    arreglo peliculas

--- SpoilerApp6/src/app/app.component.ts
+++ SpoilerApp6/src/app/app.component.ts
@@ -9,6 +9,22 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   mostrarHola = true;
+  arregloNumeros = [1, 2, 3, 4, 5, 6];
+
+  arregloPeliculas = [
+    {
+      nombre: 'Tomb Raider',
+      descripcion: 'Alicia Vikander, Walton Goggins',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg'
+    },
+    {
+      nombre: 'Ready Player One',
+      descripcion: 'Tye Sheridan, Olivia Cooke',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzFQprASMSiRnRHYgQl5sHoGr8GiMI9IB2XVIQP_tmD61KFgUY'
+    },
+  ]
 
 
   // nombre = 'Adrian';
