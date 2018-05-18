288f5a5b30720824f6649f85f419ee7fa8887ca2
Adrian Eguez <eadepto@hotmail.com>
2018-05-18 17:13:48 -0500

    ng on changes

--- SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -1,24 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pelicula-banner',
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
-export class PeliculaBannerComponent implements OnInit {
+export class PeliculaBannerComponent implements OnInit, OnChanges {
 
   @Input() urlImagen: string;
   @Input() descripcionImagen: string;
-  @Input() nombrePelicula: string; 
+  @Input() nombrePelicula: string;
   @Input() descripcionPelicula: string;
   @Input() esEstreno: boolean;
   textoEstreno: string;
   claseEstreno: string;
 
   constructor() {
+    // undefined
   }
 
   ngOnInit() {
+    console.log("Inicio y seteo color", this.nombrePelicula);
+    this.setearColorYClase();
+  }
+
+  ngOnChanges(propiedadesActualizadas) {
+    console.log('Algo', propiedadesActualizadas);
+    if (propiedadesActualizadas.esEstreno) {
+      this.setearColorYClase();
+    }
+  }
+
+  setearColorYClase() {
     if (this.esEstreno) {
       this.textoEstreno = 'Estreno';
       this.claseEstreno = 'sa-color-estado-rosado';
