fc9c5baeb2ca27826cc9c923e677591e2872aa2e
Adrian Eguez <eadepto@hotmail.com>
2018-05-22 16:15:00 -0500

    material

--- SpoilerApp6/src/app/app.module.ts
+++ SpoilerApp6/src/app/app.module.ts
@@ -6,6 +6,11 @@ import {FormsModule} from "@angular/forms";
 import { PeliculaBannerComponent } from './peliculas/pelicula-banner/pelicula-banner.component';
 import { EjercicioComponent } from './ejercicio/ejercicio.component';
 import { GrillaComponent } from './grilla/grilla.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {ButtonModule} from 'primeng/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -17,7 +22,11 @@ import { GrillaComponent } from './grilla/grilla.component';
      // Definin todos los pipes -> transforman datos
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot(),
+    ButtonModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ], //  Definimos todos los Modulos
   providers: [], // Los Servicios
   bootstrap: [AppComponent] // Componente Principal
